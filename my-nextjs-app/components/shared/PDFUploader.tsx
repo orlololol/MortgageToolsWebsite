2156396ed817a868c4549437ef2a0743547562cc
Orlando Qiu <orlandoqiu@gmail.com>
2024-08-11 22:22:38 -0400

    forgot to add use client in file

--- my-nextjs-app/components/shared/PDFUploader.tsx
+++ my-nextjs-app/components/shared/PDFUploader.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
